3e6a190fda652626b1857aa637ac7a81fb8ea4d8
Anmol <anmolkumarsinghofficial2005@gmail.com>
2025-12-21 02:34:14 +0530

    Prepare backend for Railway deployment

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,6 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-c_l7f8r-gt#de7!x-+5+yr)+f#a%&bn%17m-vdm323^2nh^j7-'
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 
@@ -180,7 +179,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -249,4 +250,6 @@ if FRONTEND_BASE_URL:
 CSRF_TRUSTED_ORIGINS = [
     os.environ.get("FRONTEND_BASE_URL"),
 ]
+
+
 
