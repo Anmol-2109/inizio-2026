510e00f05a14feee1ad20860f05b383d191c2395
Anmol <anmolkumarsinghofficial2005@gmail.com>
2025-12-22 18:47:21 +0530

    Finalize layout

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,8 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { registerForPush } from "../utils/registerpush";
 
 export default function MainLayout() {
+  const isAuthenticated = useAuthStore((s) => !!s.access);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      registerForPush();
+    }
+  }, [isAuthenticated]);
   return (
     <>
       <Navbar />
