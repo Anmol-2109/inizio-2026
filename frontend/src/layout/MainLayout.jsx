904146c2d90a39424bca0f17d3fb8ab6bbb2620a
Anmol <anmolkumarsinghofficial2005@gmail.com>
2025-12-23 10:30:59 +0530

    correct import

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { registerForPush } from "../utils/registerForPush";
+import useAuthStore from "../store/useAuthStore";
+
 
 export default function MainLayout() {
   const isAuthenticated = useAuthStore((s) => !!s.access);
