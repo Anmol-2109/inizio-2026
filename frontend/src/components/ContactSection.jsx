4319757b3a3ee67c2c75d8eea1b1bfe731bff06d
Anmol <anmolkumarsinghofficial2005@gmail.com>
2026-01-11 22:37:37 +0530

    Contact detail updated

--- frontend/src/components/ContactSection.jsx
+++ frontend/src/components/ContactSection.jsx
@@ -40,7 +40,7 @@ export default function ContactSection() {
                 <p className="contact__item-label">Phone</p>
                 <div className="contact__item-multi">
                   <a className="contact__item-link" href="tel:+918018685525">
-                    +91 18018685525
+                    +91 8018685525
                   </a>
                   <a className="contact__item-link" href="tel:+917631648529">
                     +91 7631648529
